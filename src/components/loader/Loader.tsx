95c98211bef385d3d079b3b220869bfe5c57cfb6
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-03 18:22:56 -0500

    Creating loader component

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,22 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div id="loader_container">
+      <div className="loader_item">
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
